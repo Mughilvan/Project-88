c1b014ed952d3b84ade94965b717b724e8bb8dfd
Mughilvan <83823599+Mughilvan@users.noreply.github.com>
2021-07-10 14:50:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,9 @@
 
-// Create canvas variable
-
-//Set initial positions for ball and hole images.
-
-
-block_image_width = 5;
-block_image_height = 5;
-
-function load_img(){
-	// write code to Upload golf image on the canvas
-	new_image();
-}
-
-function new_image()
-{
-	// write code to Upload ball image on canvas
-}
-
-window.addEventListener("keydown", my_keydown);
-
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	/* Check the coordinates of the ball and hole images to finish the game. 
-	And id coordinates matches them remove ball image, 
-	display "GAME OVER!!!" 
-	and make canvas border 'red'. */
+	
 	
 	else{
 		if(keyPressed == '38')
@@ -53,19 +30,18 @@ function my_keydown(e)
 	
 	function up()
 	{
-		// Write a code to move ball upward.
+		
 	}
 
 	function down()
 	{
-		 // Write a code to move ball downward.
+		 
 	}
 
 	function left()
 	{
-		if(ball_x >5)
-		{
-			// Write a code to move ball left side.
+		if(ball_x >5){
+			
 		}
 	}
 
@@ -73,7 +49,7 @@ function my_keydown(e)
 	{
 		if(ball_x <=1050)
 		{
-			// Write a code to move ball right side.
+			
 		}
 	}
 	
